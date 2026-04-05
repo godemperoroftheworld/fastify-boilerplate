374a8eb4fd86f193c8d9058d9a8756d09b657699
Tenzin Pelletier <t2pellet@uwaterloo.ca>
2026-04-05 10:02:19 -0400

    fix: remove api key stuff

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,9 @@
-import { FastifyPluginAsync, FastifyRequest } from 'fastify';
+import { FastifyPluginAsync } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
-import process from 'node:process';
-
-import { ERRORS } from '@/helpers/errors.helper';
 
 const routes: FastifyPluginAsync = async (server) => {
   const fastify = server.withTypeProvider<ZodTypeProvider>();
-  // API Keys
-  if (process.env.NODE_ENV === 'production') {
-    fastify.addHook('onRequest', async (req: FastifyRequest) => {
-      // Get known keys
-      const knownKeys = await KeyService.keys();
-      // Get passed in key
-      const apiKey = req.headers['x-api-key'];
-      if (!apiKey || typeof apiKey !== 'string' || !knownKeys.has(apiKey)) {
-        throw ERRORS.apiKey;
-      }
-    });
-  }
+  // Register routes here
 };
 
 export default routes;
